6750dc8989e25f8530e88bc9062e4c1cb7474826
Mia Liu <118697679+Mia-Liu-5577@users.noreply.github.com>
2023-07-07 11:45:52 -0700

    Create LC0121-Best-Time-to-Buy-and-Sell-Stock.py

---
+++ LC0121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # maintain the current min value
+        cur_min = prices[0]
+        max_profit = 0
+        for i in range(1, len(prices)):
+            cur_min = min(cur_min, prices[i])
+            max_profit = max(max_profit, prices[i] - cur_min)
+        return max_profit
+
